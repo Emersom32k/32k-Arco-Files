03af3b5aafc9ed290ec781fe9e19e799ff231000
Emersom32k <96273118+Emersom32k@users.noreply.github.com>
2021-12-17 14:27:08 -0300

    Update and rename after-graph-remove to after-graph-remove.sh

--- after-graph-remove.sh
+++ after-graph-remove.sh
@@ -1,11 +1,13 @@
+#!/bin/bash
+
 sudo systemctl disable tlp.service
 sudo pacman -Rs tlp --noconfirm
 sudo pacman -Rs broadcom-wl-dkms --noconfirm
 sudo pacman -Rs r8168-dkms --noconfirm
 
-echo "################################################################"
-echo "################### software removed"
-echo "################################################################"
+echo "========================"
+echo "====software removed===="
+echo "========================"
 
 sudo mkinitcpio -P
 
